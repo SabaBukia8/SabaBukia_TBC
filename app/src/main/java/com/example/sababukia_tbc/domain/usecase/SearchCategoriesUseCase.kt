e0f6103b08b6a1245be025eb4d79f7a2a2f70ebb
Saba Bukia <bukiasaba8@gmail.com>
2026-01-08 07:58:15 +0400

    small changes.

--- app/src/main/java/com/example/sababukia_tbc/domain/usecase/SearchCategoriesUseCase.kt
+++ app/src/main/java/com/example/sababukia_tbc/domain/usecase/SearchCategoriesUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.sababukia_tbc.domain.usecase
 
 import com.example.sababukia_tbc.domain.model.Category
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.map
@@ -12,6 +13,7 @@ class SearchCategoriesUseCase @Inject constructor() {
         private const val SEARCH_DEBOUNCE_MS = 300L
     }
 
+    @OptIn(FlowPreview::class)
     operator fun invoke(
         queryFlow: Flow<String>,
         categories: List<Category>
